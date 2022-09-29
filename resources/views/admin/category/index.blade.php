72135b43441d8c437859b050cd1088c559d74282
AsfandRanglerz <asfand.ranglerz@gmail.com>
2022-09-29 12:39:58 +0500

    Changes

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -162,7 +162,7 @@
     </div>
 @endsection
 @section('script')
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.js"></script>
     <script type="text/javascript">
