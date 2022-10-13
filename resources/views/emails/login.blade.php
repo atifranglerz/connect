ea41130d5a3a918532ffc3933fe6e73f1f20fb57
muhammadarshadranglerz <muhammadarshadranglerz@gmail.com>
2022-10-13 09:16:21 -0700

    register mail

--- resources/views/emails/login.blade.php
+++ resources/views/emails/login.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
 
 
-Thank you {{ $data['name'] }}. You are now a part of a community that connects people. Find a  service providers or search the right service  and. Log-on to your member page at:
+Thank you {{ $data['name'] }}. Your submission will be reviewed by Repair My Car team, and on the basis of that soon your account will be activated and we'll send you a activatation email as a reminder.
 
-@component('mail::button', ['url' =>  $data['link']])
+{{-- @component('mail::button', ['url' =>  $data['link']])
     Login To Dashboard
-    @endcomponent
+    @endcomponent --}}
 <br>
 In case you need to talk, please call us at our support +971 56 784 5425<br>
 Or send us a mail at <a href="support@repairmycar.com">support@repairmycar.com</a><br>
