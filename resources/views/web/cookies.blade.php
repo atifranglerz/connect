d2c3a0251159721fe17f44fadfa3d2a42737ff0f
AsfandRanglerz <asfand.ranglerz@gmail.com>
2022-10-17 18:42:29 +0500

    changes

--- resources/views/web/cookies.blade.php
+++ resources/views/web/cookies.blade.php
@@ -1,5 +1,26 @@
 @extends('web.layout.app')
 @section('content')
-
-fsfgsdgsd
+    <style>
+        .admin-pages h1, .admin-pages h2, .admin-pages h3, .admin-pages h4, .admin-pages h5, .admin-pages h6 {
+            text-transform: uppercase;
+            color: var(--orange);
+        }
+        .admin-pages b, .admin-pages strong {
+            font-family: Inter,sans-serif!important;
+        }
+        .admin-pages p {
+            text-align: justify;
+        }
+        @media (max-width: 575px) {
+            .admin-pages h1, .admin-pages h2, .admin-pages h3, .admin-pages h4, .admin-pages h5, .admin-pages h6 {
+                text-align: center;
+            }
+        }
+    </style>
+    <section class="admin-pages about_connect looking_for lates_news_main">
+        <div class="container-lg container-fluid">
+            <h4 class="sec_main_heading text-center">{{__('msg.Cookie Policy')}}</h4>
+            {!! $cookiePolicy->description !!}
+        </div>
+    </section>
 @endsection
