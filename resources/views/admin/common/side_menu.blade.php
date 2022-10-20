ef0723fb34ab415b52470fdc9ada803492b5f218
muhammadarshadranglerz <muhammadarshadranglerz@gmail.com>
2022-10-20 07:07:37 -0700

    e

--- resources/views/admin/common/side_menu.blade.php
+++ resources/views/admin/common/side_menu.blade.php
@@ -13,80 +13,80 @@
                 <a href="{{ route('admin.dashboard') }}" class="nav-link"><i
                         data-feather="monitor"></i><span>Dashboard</span></a>
             </li>
+            <li class="dropdown @if (request()->routeIs('admin.order*')) active @endif">
+                <a href="#" class="menu-toggle nav-link has-dropdown">
+                    <i data-feather="tag"></i><span>Orders</span></a>
+                <ul class="dropdown-menu">
+                    <li class=""><a class="nav-link " href="{{ route('admin.order.index') }}">All Orders</a></li>
+                </ul>
+            </li>
             <li class="dropdown @if (request()->routeIs(
-                'admin.garage.index',
-                'admin.user.index',
-                'admin.category.edit',
-                'admin.category.show',
-                'admin.subcategory.index',
-                'admin.subcategory.create',
-                'admin.subcategory.edit',
-                'admin.subcategory.show',
-                'admin.childcategory.index',
-                'admin.childcategory.create',
-                'admin.childcategory.edit',
-                'admin.childcategory.show',
-                'admin.order.index',
-                'admin.order.create',
-                'admin.order.edit',
-                'admin.ads.index',
-                'admin.insurance-company',
-                'admin.vendor.index',
-                'admin.category.index')) active @endif">
+                'admin.garage*',
+                'admin.addUser',
+                'admin.user*',
+                'admin.ads*',
+                'admin.insurance*',
+                'admin.vendor*',
+                // 'admin.subcategory*',
+                // 'admin.childcategory*',
+                'admin.category*')) active @endif">
                 <a href="#" class="menu-toggle nav-link has-dropdown">
                     <i data-feather="tag"></i><span>Master List</span></a>
                 <ul class="dropdown-menu">
                     <li class=""><a class="nav-link " href="{{ route('admin.garage.index') }}">Garages</a></li>
                     <li class=""><a class="nav-link " href="{{ route('admin.user.index') }}">Customers</a></li>
                     <li class=""><a class="nav-link " href="{{ route('admin.ads.index') }}">Car Ads</a></li>
-                    <li class=""><a class="nav-link " href="{{ route('admin.insurance-company') }}">Insurance
+                    <li class=""><a class="nav-link " href="{{ route('admin.insurance.index') }}">Insurance
                             Company</a></li>
                     <li class=""><a class="nav-link " href="{{ route('admin.vendor.index') }}">Vendors</a></li>
                     <li class=""><a class="nav-link " href="{{ route('admin.category.index') }}">Services</a></li>
-                    <li class=""><a class="nav-link " href="{{ route('admin.order.index') }}">Orders</a></li>
                 </ul>
             </li>
             <li class="dropdown @if (request()->routeIs(
                 'admin.simpleAd*',
-                'admin.all-packages',
-                'admin.news.index',
-                'admin.about.index',
-                'admin.cookie.index',
-                'admin.cookie.edit',
-                'admin.cookie.store',
-                '/admin/faqs',
+                'admin.slider*',
+                'admin.news*',
+                'admin.about*',
+                'admin.faq.index',
+                'admin.faq.add',
+                'admin.faq.edit',
                 'admin.term.index',
+                'admin.term.edit',
                 'admin.privacyPolicy.index',
-                'admin.childcategory.show')) active @endif">
+                'admin.privacyPolicy.edit',
+                'admin.cookie*')) active @endif">
                 <a href="#" class="menu-toggle nav-link has-dropdown">
                     <i data-feather="tag"></i><span>Main Settings</span></a>
                 <ul class="dropdown-menu">
-                    {{-- <li class=""><a class="nav-link " href="{{ route('admin.garage.index') }}">Garages</a></li> --}}
                     <li class="dropdown @if (request()->routeIs(
-                        'admin.news.index',
                         'admin.simpleAd*',
-                        'admin.all-packages',
-                        'admin.cookie.index',
-                        'admin.cookie.edit',
-                        'admin.cookie.store',
-                        'admin.about.index',
-                        '/admin/faqs',
+                        'admin.slider*',
+                        'admin.news*',
+                        'admin.about*',
+                        'admin.faq.index',
+                        'admin.faq.add',
+                        'admin.faq.edit',
                         'admin.term.index',
+                        'admin.term.edit',
                         'admin.privacyPolicy.index',
-                        'admin.childcategory.show')) active @endif">
+                        'admin.privacyPolicy.edit',
+                        'admin.all-packages',
+                        'admin.cookie*')) active @endif">
                         <a href="#" class="menu-toggle nav-link has-dropdown">
                             <i data-feather="tag"></i><span>Site Settings</span></a>
                         <ul class="dropdown-menu">
                             <li class="dropdown @if (request()->routeIs(
-                                'admin.news.index',
-                                'admin.about.index',
-                                'admin.cookie.index',
-                                'admin.cookie.edit',
-                                'admin.cookie.store',
-                                '/admin/faqs',
+                                'admin.slider*',
+                                'admin.news*',
+                                'admin.about*',
+                                'admin.faq.index',
+                                'admin.faq.add',
+                                'admin.faq.edit',
                                 'admin.term.index',
+                                'admin.term.edit',
                                 'admin.privacyPolicy.index',
-                                'admin.childcategory.show')) active @endif">
+                                'admin.privacyPolicy.edit',
+                                'admin.cookie*')) active @endif">
                                 <a href="#" class="menu-toggle nav-link has-dropdown">
                                     <i data-feather="tag"></i><span>Pages</span></a>
                                 <ul class="dropdown-menu">
@@ -107,8 +107,8 @@
                                 </ul>
                             </li>
                             <li class="dropdown @if (request()->routeIs('admin.simpleAd*', 'admin.all-packages')) active @endif">
-                                {{-- <a href="#" class="menu-toggle nav-link has-dropdown">
-                                    <i data-feather="tag"></i><span>Simple Ads</span></a> --}}
+                                <a href="#" class="menu-toggle nav-link has-dropdown">
+                                    <i data-feather="tag"></i><span>Simple Ads</span></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="{{ route('admin.simpleAd.index') }}" class="nav-link">All Ads</a>
                                     </li>
@@ -120,11 +120,15 @@
                     </li>
                 </ul>
             </li>
+
             <li class="dropdown @if (request()->routeIs(
-                'admin.company.index',
-                'admin.brand.index',
-                'admin.model_year.index',
-                'admin.childcategory.show')) active @endif">
+                'admin.company*',
+                'admin.brand*',
+                'admin.car-model',
+                'admin.edit-model',
+                'admin.view-model',
+                'admin.model_year*',
+                'admin.childcategory*')) active @endif">
                 <a href="#" class="menu-toggle nav-link has-dropdown">
                     <i data-feather="tag"></i><span>Car Setup</span></a>
                 <ul class="dropdown-menu">
@@ -135,28 +139,16 @@
                     </li>
                 </ul>
             </li>
-            <li class="dropdown @if (request()->routeIs(
-                'admin.company.index',
-                'admin.brand.index',
-                'admin.model_year.index',
-                'admin.childcategory.show')) active @endif">
+            <li class="dropdown @if (request()->routeIs('admin.percentage.index', 'admin.percentage.edit')) active @endif">
                 <a href="#" class="menu-toggle nav-link has-dropdown">
                     <i data-feather="tag"></i><span>Setting</span></a>
                 <ul class="dropdown-menu">
-                    {{-- <li class=""><a class="nav-link " href="{{ route('admin.company.index') }}">Car Company Name</a></li>
-                    <li class=""><a class="nav-link " href="{{ route('admin.brand.index') }}">Brands</a></li>
-                    <li class=""><a class="nav-link " href="{{ route('admin.model_year.index') }}">Model Year</a></li> --}}
-                    <li class=""><a class="nav-link " href="{{ url('/admin/percentage') }}">Payment and Vat</a>
+                    {{-- <li class=""><a class="nav-link " href="{{ route('admin.brand.index') }}">Brands</a></li> --}}
+                    <li class=""><a class="nav-link " href="{{ url('/admin/percentage') }}">Payment and
+                            Vat</a>
                     </li>
                 </ul>
             </li>
-            {{-- <li class="dropdown @if (request()->routeIs('admin.order.index', 'admin.order.create', 'admin.order.edit')) active @endif">
-                <a href="#" class="menu-toggle nav-link has-dropdown">
-                    <i data-feather="shopping-cart"></i><span>Orders</span></a>
-                <ul class="dropdown-menu">
-                    <li class=""><a class="nav-link " href="{{ route('admin.order.index') }}">Orders</a></li>
-                </ul>
-            </li> --}}
         </ul>
     </aside>
 </div>
